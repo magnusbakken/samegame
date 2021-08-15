1fda871037f55738ad7b4e2f59a27050e2f1b599
Magnus Grindal Bakken <magnusbakken@gmail.com>
2021-08-15 15:43:14 +0200

    More elm stuff

--- src/Main.elm
+++ src/Main.elm
@@ -3,28 +3,86 @@ module Main exposing (..)
 
 import Browser
 import Html exposing (Html, button, div, text)
+import Html.Attributes exposing (class)
 import Html.Events exposing (onClick)
+import Random
 
-main =
-  Browser.sandbox { init = 0, update = update, view = view }
+main = Browser.element {
+    init = init,
+    update = update,
+    subscriptions = subscriptions,
+    view = view
+    }
 
-type Msg = Increment | Decrement
+-- Model
 
+tableHeight = 10
+tableWidth = 20
+
+type Color = Red | Green | Blue | Yellow | Empty
+
+type alias Model = {
+    colors : List (List Color)
+    }
+
+type Msg =
+    RandomizeColors |
+    SetColors (List (List Color))
+
+init : () -> (Model, Cmd Msg)
+init _ = update RandomizeColors (Model (List.repeat tableHeight (List.repeat tableWidth Empty)))
+
+-- Update
+
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    Increment ->
-      model + 1
+    RandomizeColors -> (
+        model,
+        Random.generate SetColors (randomColors tableHeight tableWidth)
+        )
+    SetColors colors -> (
+        Model colors,
+        Cmd.none
+        )
 
-    Decrement ->
-      model - 1
+randomColor : Random.Generator Color
+randomColor = Random.uniform Red [Green, Blue, Yellow]
 
-view model =
-  div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (String.fromInt model) ]
-    , button [ onClick Increment ] [ text "+" ]
-    ]
+splitList : Int -> List a -> List (List a)
+splitList len l = case l of
+    [] -> []
+    list -> List.take len list :: splitList len (List.drop len list)
 
-type Color = Red | Green | Blue | Yellow
+makeTable : Int -> Int -> List Color -> List (List Color)
+makeTable height width list = splitList width list
 
---cell
+randomColors : Int -> Int -> Random.Generator (List (List Color))
+randomColors height width = Random.map (makeTable height width) (Random.list (height * width) randomColor)
+
+-- Subscriptions
+
+subscriptions : Model -> Sub Msg
+subscriptions model = Sub.none
+
+-- View
+
+view : Model -> Html Msg
+view model = fullTable model.colors
+
+colorClass : Color -> String
+colorClass color = case color of
+    Red -> "red"
+    Green -> "green"
+    Blue -> "blue"
+    Yellow -> "yellow"
+    Empty -> "empty"
+
+tableCell : Color -> Html Msg
+tableCell color = div [ class "cell", class (colorClass color) ] [ text "" ]
+
+tableRow : List Color -> Html Msg
+tableRow colors = div [ class "row" ] (List.map tableCell colors)
+
+fullTable : List (List Color) -> Html Msg
+fullTable allColors = div [ class "game" ] (List.map tableRow allColors)
